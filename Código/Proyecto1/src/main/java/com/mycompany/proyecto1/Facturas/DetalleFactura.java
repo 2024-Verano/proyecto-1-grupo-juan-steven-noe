df51aafeee69023fc4ed4f422c8f69ba4ca867a9
nlopez2003 <noelopes100@gmail.com>
2024-12-30 16:23:34 -0600

    Se empaquetaron los archivos de forma ordenada.

--- Código/Proyecto1/src/main/java/com/mycompany/proyecto1/Facturas/DetalleFactura.java
+++ Código/Proyecto1/src/main/java/com/mycompany/proyecto1/Facturas/DetalleFactura.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
-package com.mycompany.proyecto1.facturacion;
+package com.mycompany.proyecto1.Facturas;
 import com.fasterxml.jackson.annotation.JsonProperty;
 /**
  *
